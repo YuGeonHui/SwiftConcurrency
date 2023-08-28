294c2aa45b93ea46a46c08f8a4f93a7952b79a58
YuGeonHui <ghyu_0906@naver.com>
2023-08-28 10:45:59 +0900

    Initial Commit

---
+++ ConcurrencyBootcamp/ConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyBootcampApp.swift
+//  ConcurrencyBootcamp
+//
+//  Created by geonhui Yu on 2023/08/28.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
